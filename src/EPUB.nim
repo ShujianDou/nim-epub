8dde409252317b46af8ab1a31a461dfa83a147f7
ShujianDou <visionistprojects@gmail.com>
2022-08-07 14:49:25 -0400

    version bump, minor updates

--- src/EPUB.nim
+++ src/EPUB.nim
@@ -42,6 +42,7 @@ method EndEpubExport*(this: Epub, bookID: string, publisher: string, cover: stri
   this.opfMetaData.metaDataObjects.add(Meta(content: (">$1" % [EscapeValues(this.title)]), name: "title", metaType: MetaType.dc))
   this.opfMetaData.metaDataObjects.add(Meta(content: ">en_US", name: "language", metaType: MetaType.dc))
   this.opfMetaData.metaDataObjects.add(Meta(content: "opf:role=\"auth\" opf:file-as=\"$1\">$1" % [EscapeValues(this.author)], name: "creator", metaType: MetaType.dc))
+  this.opfMetaData.metaDataObjects.add(Meta(content: "opf:role=\"aut\" opf:file-as=\"$1\">$1" % [EscapeValues(this.author)], name: "creator", metaType: MetaType.dc))
   this.opfMetaData.metaDataObjects.add(Meta(content: "id=\"BookID\" opf:scheme=\"URI\">$1" % [bookID], name: "identifier", metaType: MetaType.dc))
   this.opfMetaData.metaDataObjects.add(Meta(content: ">$1" % [EscapeValues(publisher)], name: "publisher", metaType: MetaType.dc))
   this.opfMetaData.metaDataObjects.add(Meta(content: "cover", name: "cover", metaType: MetaType.meta))
@@ -58,14 +59,14 @@ method EndEpubExport*(this: Epub, bookID: string, publisher: string, cover: stri
   if cover != "":
     manifest.items.add(Item(id: "cover", href: "../cover.jpeg", mediaType: MediaType.pImage))
     writeFile(this.filePath & "/cover.jpeg", cover)
-  manifest.items.add(Item(id: "ncx", href: "toc.ncx", mediaType: MediaType.pImage))
+  manifest.items.add(Item(id: "ncx", href: "toc.ncx", mediaType: MediaType.pNcx))
   var spine: Spine = Spine(items: manifest.items)
   this.opf = OPFPackage(metaData: this.opfMetaData, manifest: manifest, spine: spine)
   #Export Finalize
   writeFile(this.filePath & "/OEBPS/content.opf", this.opf.ToString())
   writeFile(this.filePath & "/OEBPS/toc.ncx", this.tableOfContents.ToString())
   writeFile(this.filePath & "/OEBPS/cover.xhtml", "<?xml version='1.0' encoding='utf-8'?><html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta name=\"calibre:cover\" content=\"true\"/><title>Cover</title><style type=\"text/css\" title=\"override_css\">@page {padding: 0pt; margin:0pt}\nbody { text-align: center; padding:0pt; margin: 0pt; }</style></head><body><div><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" width=\"100%\" height=\"100%\" viewBox=\"0 0 741 1186\" preserveAspectRatio=\"none\"><image width=\"741\" height=\"1186\" xlink:href=\"../cover.jpeg\"/></svg></div></body></html>")
-  createZipArchive(this.filePath, this.filePath & ".epub")
+  createZipArchive(this.filePath & "/", this.filePath & ".epub")
   removeDir(this.filePath)
   return true
 
