7e06427f0cc115e3e1b7ed655d665fa1dd706682
ShujianDou <visionistprojects@gmail.com>
2022-08-17 15:27:24 -0400

    update readme and epub.nim

--- src/EPUB.nim
+++ src/EPUB.nim
@@ -1,9 +1,7 @@
-import zippy/ziparchives
-import std/[xmltree, streams, times, os, strutils, base64]
-import EPUB/EPUB3, EPUB/types
+import EPUB/[types, EPUB3]
 
 block:
-  var lmfao: EPUB3
+  var testEpub: EPUB3
   let mdataList: seq[metaDataList] = @[
     (metaType: MetaType.dc, name: "title", attrs: @[("id", "title")], text: "hellow"),
     (metaType: MetaType.dc, name: "creator", attrs: @[("id", "creator")], text: "melul"),
@@ -11,6 +9,6 @@ block:
     (metaType: MetaType.dc, name: "identifier", attrs: @[("id", "pub-id")], text: "helloworld"),
     (metaType: MetaType.meta, name: "", attrs: @[("property", "dcterms:modified")], text: "2022-01-02T03:50:100"),
     (metaType: MetaType.dc, name: "publisher", attrs: @[], text: "vrienco")]
-  lmfao = CreateEpub3(mdataList, "/mnt/General/work/Programming/EPUB/src/helloCuteWorld")
-  AddPage(lmfao, GeneratePage("Hello", @[TiNode(text: "helloWorld")]))
-  FinalizeEpub(lmfao)
+  testEpub = CreateEpub3(mdataList, "/mnt/General/work/Programming/EPUB/src/helloCuteWorld")
+  AddPage(testEpub, GeneratePage("dlrowolleh", @[TiNode(text: "helloWorld")]))
+  FinalizeEpub(testEpub)
